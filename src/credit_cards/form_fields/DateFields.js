605e6ce195c4d84bc7e2a04bea2a6a302f1afba0
Christine Casebolt <christine.casebolt@gmail.com>
2018-04-21 13:20:06 -0700

    date field component

--- src/credit_cards/form_fields/DateFields.js
+++ src/credit_cards/form_fields/DateFields.js
@@ -1,6 +1,7 @@
 // @flow
 import React from "react";
 import InputField from "../../base_components/InputField";
+import { isDate } from "util";
 
 type Props = {
   onMonthChange: string => void,
@@ -10,23 +11,77 @@ type Props = {
 };
 type State = {};
 export default class DateFields extends React.Component<Props, State> {
+  handleMonthChange = (month: string) => {
+    const newMonth = restrictedDateInput(processMonth(month));
+    this.props.onMonthChange(newMonth);
+  };
+  handleYearChange = (year: string) => {
+    const newYear = restrictedDateInput(year);
+    this.props.onYearChange(newYear);
+  };
   render() {
-    const { onMonthChange, onYearChange, month, year } = this.props;
+    const { onYearChange, month, year } = this.props;
     return (
       <div>
         <InputField
           type="number"
           value={month}
-          onChange={onMonthChange}
+          onChange={this.handleMonthChange}
           placeholder="month"
         />
         <InputField
           value={year}
           type="number"
-          onChange={onYearChange}
+          onChange={this.handleYearChange}
           placeholder="year"
         />
+        {!isDateValid(month, year) ? "Invalid date" : null}
       </div>
     );
   }
+}
+
+function restrictedDateInput(dateInput: string): string {
+  if (dateInput.length > 2) {
+    return dateInput.slice(0, 2);
+  }
+  return dateInput;
+}
+
+function processMonth(month: string): string {
+  if (month.length === 1 && /^[2-9]$/.test(month[0])) {
+    return "0".concat(month);
+  }
+  if (month[0] !== "0" && /^[3-9]$/.test(month[1])) {
+    return month[0];
+  }
+  return month;
+}
+
+function isDateValid(month: string | null, year: string | null): boolean {
+  if (!year || (year && year.length < 2)) {
+    return true;
+  }
+  // note to self - this will break on Jan 1, 2100
+  const inputYearValue = parseInt("20" + year, 10);
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  // basic year check
+  if (inputYearValue < currentYear) {
+    return false;
+  }
+
+  // month check
+  if (!month || (month && month.length < 2)) {
+    return true;
+  }
+  const inputMonthValue = parseInt(month, 10);
+  if (
+    inputYearValue === currentYear &&
+    inputMonthValue < currentDate.getMonth()
+  ) {
+    return false;
+  }
+  return true;
 }
